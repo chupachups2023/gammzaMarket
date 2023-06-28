265e3e2c270ab1388310426771706938724ee9b7
Jun <watermoonxx@gmail.com>
2023-06-28 19:13:15 +0900

    member 관련 업로드

---
+++ src/main/java/com/gammza/chupachups/member/model/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.gammza.chupachups.member.model.service;
+
+import com.gammza.chupachups.member.model.vo.Member;
+
+public interface MemberService {
+	
+	int insertMember(Member member);
+	
+	Member selectOneMember(String userId);
+	
+	int updateMember(Member member);
+
+}
