01a5b2f2f9e255d5050126bc716bd8dcd159dd92
junsu2023 <yjsu2009@naver.com>
2023-06-28 11:40:03 +0900

    컨트롤러
    notice컨트롤러

---
+++ src/main/java/com/gammza/chupachups/notice/controller/model/dao/NoticeDao.java
@@ -0,0 +1,26 @@
+package com.gammza.chupachups.notice.controller.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.gammza.chupachups.notice.controller.model.vo.Notice;
+
+
+
+@Mapper
+public interface NoticeDao {
+	List<Notice> selectnoticeList(RowBounds rowBounds);
+	
+	int selectTotalRecord();
+
+	int writeNotice(Notice notice, RedirectAttributes rd);
+
+	int deleteNotice(int noticeNo, RedirectAttributes rd);
+
+	Object selectOneNotice(int noticeNo);
+
+	int updateNotice(Notice notice);
+}
