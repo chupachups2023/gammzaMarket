e3471c639f4f10c8d7b73b863ddf0381e3d4fe29
yuna <chupachups20230720@gmail.com>
2023-06-28 12:16:09 +0900

    공구 백앤드 패키지

--- src/main/java/com/gammza/chupachups/gonggu/controller/GongguController.java
+++ src/main/java/com/gammza/chupachups/gonggu/controller/GongguController.java
@@ -1,13 +1,15 @@
 package com.gammza.chupachups.gonggu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gammza.chupachups.gonggu.model.service.GongguService;
 
 @Controller
 @RequestMapping("/gonggu")
 public class GongguController {
+	@Autowired
+	private GongguService gongguService;
 	
-	@GetMapping("ggEnd.re")
-	public void ggEnd() {}
 }
---
+++ src/main/java/com/gammza/chupachups/gonggu/model/vo/Gonggu.java
@@ -0,0 +1,33 @@
+package com.gammza.chupachups.gonggu.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor 
+@ToString
+public class Gonggu {
+	private int gongguNo;
+	private String gongguName;
+	private String gongguWriter;
+	private int price;
+	private int type;
+	private int num;
+	private int category;
+	private String content;
+	private String photo1;
+	private String photo2;
+	private String photo3;
+	private String link;
+	private String createAt;
+	private String sendTime;
+	private String openTime;
+	private String endTime;
+	private String latitude;
+	private String longitude;
+	private int count;
+	private int status;
+}
