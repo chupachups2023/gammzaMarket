6c12a46734c344cbb75e2bcacd7ecabde30cd444
suhyunchoi96 <kr.suhyunchoi96@gmail.com>
2023-06-28 17:50:56 +0900

    Create GongguServiceImpl.java

---
+++ src/main/java/com/gammza/chupachups/gonggu/model/service/GongguServiceImpl.java
@@ -0,0 +1,13 @@
+package com.gammza.chupachups.gonggu.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gammza.chupachups.gonggu.model.dao.GongguDao;
+
+@Service
+public class GongguServiceImpl implements GongguService {
+	
+	@Autowired
+	private GongguDao gongguDao;
+}
