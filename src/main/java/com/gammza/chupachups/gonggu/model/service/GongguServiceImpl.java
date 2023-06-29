03a5656564ab7126c1fdd880dbbb5f2e7dec1959
yuna <chupachups20230720@gmail.com>
2023-06-29 22:23:50 +0900

    글 업로드하면 바로 글 확인할 수 있도록

--- src/main/java/com/gammza/chupachups/gonggu/model/service/GongguServiceImpl.java
+++ src/main/java/com/gammza/chupachups/gonggu/model/service/GongguServiceImpl.java
@@ -1,14 +1,10 @@
 package com.gammza.chupachups.gonggu.model.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gammza.chupachups.gonggu.model.dao.GongguDao;
 import com.gammza.chupachups.gonggu.model.vo.Gonggu;
-
-import com.gammza.chupachups.gonggu.model.vo.Parti_Tb;
 
 @Service
 public class GongguServiceImpl implements GongguService {
@@ -22,12 +18,17 @@ public class GongguServiceImpl implements GongguService {
 	}
 
 	@Override
-	public Gonggu selectOneGonggu(Gonggu gonggu) {
-		return gongguDao.selectOneGonggu(gonggu);
+	public Gonggu selectOneGonggu(int gongguNo) {
+		return gongguDao.selectOneGonggu(gongguNo);
 	}
 
 	@Override
 	public Gonggu selectOneHomeList() {
 		return gongguDao.selectOneHomeList();
+	}
+
+	@Override
+	public int selectLastNum() {
+		return gongguDao.selectLastNum();
 	}
 }
