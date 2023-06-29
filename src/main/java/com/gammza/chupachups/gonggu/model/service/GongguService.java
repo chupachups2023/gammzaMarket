76e1c88fae587d340013c481486350114e059876
suhyunchoi96 <kr.suhyunchoi96@gmail.com>
2023-06-29 13:52:08 +0900

    Update GongguService.java

--- src/main/java/com/gammza/chupachups/gonggu/model/service/GongguService.java
+++ src/main/java/com/gammza/chupachups/gonggu/model/service/GongguService.java
@@ -1,5 +1,13 @@
 package com.gammza.chupachups.gonggu.model.service;
 
+import java.util.List;
+
+import com.gammza.chupachups.gonggu.model.vo.Gonggu;
+import com.gammza.chupachups.gonggu.model.vo.Parti_Tb;
+
 public interface GongguService {
 
+	int insertGonggu(Gonggu gonggu);
+
+	Parti_Tb selectOneParti_Tb(int gongguNo);
 }
