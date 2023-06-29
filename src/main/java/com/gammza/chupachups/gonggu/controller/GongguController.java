c7f68cc7841097a0000c1abe8be23461f691d714
suhyunchoi96 <kr.suhyunchoi96@gmail.com>
2023-06-29 13:51:54 +0900

    Update GongguController.java

--- src/main/java/com/gammza/chupachups/gonggu/controller/GongguController.java
+++ src/main/java/com/gammza/chupachups/gonggu/controller/GongguController.java
@@ -1,17 +1,25 @@
 package com.gammza.chupachups.gonggu.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.gammza.chupachups.gonggu.model.service.GongguService;
 import com.gammza.chupachups.gonggu.model.vo.Gonggu;
-
-import com.gammza.chupachups.gonggu.model.vo.Gonggu;
+import com.gammza.chupachups.gonggu.model.vo.Parti_Tb;
 
 @Controller
 @RequestMapping("/gonggu")
@@ -27,9 +35,11 @@ public class GongguController {
 	
 
 	@GetMapping("/ggRead_Partic.go")
-	public String ggRead_Partic() {
-	
-		return null;
+	public String ggRead_Partic(Model model, @RequestParam int gongguNo) {
+
+		Parti_Tb parti_tb  = gongguService.selectOneParti_Tb(gongguNo);
+		model.addAttribute("parti_tb", parti_tb);
+		return "ggRead_Partic";
 	}
 	
 
