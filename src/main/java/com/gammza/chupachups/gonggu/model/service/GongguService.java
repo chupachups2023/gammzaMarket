ab14915229d49d0d5e9379ec2d768e7986e13bbf
suhyunchoi96 <kr.suhyunchoi96@gmail.com>
2023-07-02 22:58:05 +0900

    Update GongguService.java

--- src/main/java/com/gammza/chupachups/gonggu/model/service/GongguService.java
+++ src/main/java/com/gammza/chupachups/gonggu/model/service/GongguService.java
@@ -13,5 +13,15 @@ public interface GongguService {
 
 	Gonggu selectOneGonggu(Gonggu gonggu);
 
+<<<<<<< Updated upstream
 	Gonggu selectOneHomeList();
+=======
+	ArrayList<Gonggu> selectHomeList();
+
+	int selectLastNum();
+
+	int selectTotalRecored();
+
+	ArrayList<Gonggu> selectggListView();
+>>>>>>> Stashed changes
 }
