6bb605a466b7f98730b5f4f7bfae62c8491d8f82
yuna <chupachups20230720@gmail.com>
2023-07-04 19:41:01 +0900

    찜 업데이트

--- src/main/java/com/gammza/chupachups/likeList/controller/LikeListController.java
+++ src/main/java/com/gammza/chupachups/likeList/controller/LikeListController.java
@@ -49,4 +49,8 @@ public class LikeListController {
 		}
 		return "redirect:/mypage/likeList.do";
 	}
+	
+	public List<Zzim> selectZzim(int gongguNo) {
+		return likeListService.selectZzim(gongguNo);
+	}
 }
--- src/main/java/com/gammza/chupachups/likeList/model/service/LikeListService.java
+++ src/main/java/com/gammza/chupachups/likeList/model/service/LikeListService.java
@@ -2,12 +2,14 @@ package com.gammza.chupachups.likeList.model.service;
 
 import java.util.List;
 
+import org.apache.ibatis.session.RowBounds;
+
 import com.gammza.chupachups.likeList.model.vo.Zzim;
 
 public interface LikeListService {
 
 	int selectTotalRecord(String userId);
-
+ 
 	List<Zzim> selectLikeList(String userId, RowBounds rowBounds);
 
 	int deleteZzim(String zzimNo);
