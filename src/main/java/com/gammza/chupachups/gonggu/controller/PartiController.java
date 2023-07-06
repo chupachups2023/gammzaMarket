c73dbb0381c18a832574e49fe7ba6c7b88ce4ea4
yuna <chupachups20230720@gmail.com>
2023-07-06 11:06:47 +0900

    공구 참여하기

--- src/main/java/com/gammza/chupachups/gonggu/controller/PartiController.java
+++ src/main/java/com/gammza/chupachups/gonggu/controller/PartiController.java
@@ -1,23 +1,39 @@
 package com.gammza.chupachups.gonggu.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gammza.chupachups.gonggu.model.service.GongguService;
 import com.gammza.chupachups.gonggu.model.service.PartiService;
 import com.gammza.chupachups.gonggu.model.vo.Gonggu;
+import com.gammza.chupachups.gonggu.model.vo.Parti;
+import com.gammza.chupachups.location.model.service.LocationService;
+import com.gammza.chupachups.member.model.service.MemberService;
+import com.gammza.chupachups.member.model.vo.Member;
+
 
 @Controller
 @RequestMapping("/gonggu")
+@SessionAttributes({"loginMember"})
 public class PartiController {
 	@Autowired
 	private PartiService partiService;
 	@Autowired
 	private GongguService gongguService;
+	@Autowired
+	private LocationService locationService;
+	@Autowired
+	private MemberService memberService;
+
 	
 	@GetMapping("/partiEnroll.pa")
 	public String partiEnroll(@RequestParam int gongguNo, Model model) {
@@ -27,5 +43,26 @@ public class PartiController {
 		return "/gonggu/ggPartiEnroll";
 	}
 	
+	@PostMapping("/partiEnroll.pa")
+	public String partiEnrollFrm(Parti parti,RedirectAttributes redirectAttr,Model model) {
+		int result=partiService.insertParti(parti);
+		
+		if(result>0) {
+			int totalPrice=gongguService.selectOneGonggu(parti.getGongguNo()).getPrice()*parti.getNum();
+			HashMap<String,String> updatePoint=new HashMap<String,String>();
+			updatePoint.put("totalPoint", String.valueOf(totalPrice));
+			updatePoint.put("userId", parti.getPartiMember());
+			int pointResult=partiService.updateMemberPoint(updatePoint);
+			if(pointResult>0) {
+				Member member=memberService.selectOneMember(parti.getPartiMember());
+				model.addAttribute("loginMember", member);
+			}
+			return "/gonggu/ggPartiList";
+		}else {
+			redirectAttr.addFlashAttribute("msg","참여가 정상적으로 이루어지지 않았습니다.");
+			return "/home";
+		}
+		
+	}
 	
 }
