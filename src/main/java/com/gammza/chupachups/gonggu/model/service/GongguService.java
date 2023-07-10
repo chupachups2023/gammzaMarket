14bab5d8b9f4d3261078aecbcd0c5534c61a48cc
chupachups2023 <137775249+chupachups2023@users.noreply.github.com>
2023-07-10 10:36:04 +0900

    Update GongguService.java

--- src/main/java/com/gammza/chupachups/gonggu/model/service/GongguService.java
+++ src/main/java/com/gammza/chupachups/gonggu/model/service/GongguService.java
@@ -10,37 +10,31 @@ import com.gammza.chupachups.gonggu.model.vo.Gonggu;
 
 public interface GongguService {
 	
-	ArrayList<Gonggu> selectHomeList();
-	
-	ArrayList<Gonggu> selectggListView(HashMap<String,String> locationMap);
-
-	int insertGonggu(Gonggu gonggu);
-
-	Gonggu selectOneGonggu(int gongguNo);
-
-	int selectLastNum();
-
-	int selectTotalRecored();
-
-	int updateGongguCount(int gongguNo);
-
-	int updateEndStatus(int gongguNo);
-
-	int updateGonggu(Gonggu gonggu);
-
-	ArrayList<Gonggu> selectLeadGongguList(String userId);
-
-
-	int updateGongguCount(int gongguNo);
-
-	int updateEndStatus(int gongguNo);
-
-	int updateGonggu(Gonggu gonggu);
-
 	ArrayList<Gonggu> searchGonggu(String gongguName);
 	
-	int selectGongguTotalRecored();
-
+	ArrayList<Gonggu> selectggListView(HashMap<String,String> locationMap);
+	
+	ArrayList<Gonggu> selectHomeList();
+	
+	ArrayList<Gonggu> selectLeadGongguList(String userId);
+	
 	ArrayList<Gonggu> selectOneCategory(int category);
+	
+	Gonggu selectOneGonggu(int gongguNo);
+	
+	int insertGonggu(Gonggu gonggu);
+	
+	int selectGongguTotalRecored();
+	
+	int selectLastNum();
+	
+	int selectTotalRecored();
+	
+	int updateEndStatus(int gongguNo);
+	
+	int updateGonggu(Gonggu gonggu);
+	
+	int updateGongguCount(int gongguNo);
+
 
 }
