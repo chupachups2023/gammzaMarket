4d09c2a362ee4d1343874ccc3cc23475dc749d25
Jun <watermoonxx@gmail.com>
2023-07-14 21:51:48 +0900

    Update MemberDao.java

--- src/main/java/com/gammza/chupachups/member/model/dao/MemberDao.java
+++ src/main/java/com/gammza/chupachups/member/model/dao/MemberDao.java
@@ -37,8 +37,8 @@ public interface MemberDao {
 
 	String findPwdClick(String userId, String phone);
 
-	int updatePwd(Member member);
-
 	Member selectOneMemberByEmail(String email);
+
+	int updatePwd(Member tempMember);
 
 }
