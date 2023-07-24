acfeac3321b554ec6c59f23433ef59a330e6aea3
chupachups2023 <137775249+chupachups2023@users.noreply.github.com>
2023-07-24 16:10:13 +0900

    지도 서비스키 삭제

--- src/main/webapp/resources/js/gonggu/ggWriteMap.js
+++ src/main/webapp/resources/js/gonggu/ggWriteMap.js
@@ -12,7 +12,7 @@
         	type:"get",
         	url:"https://dapi.kakao.com/v2/local/geo/coord2address.json?x="+longitude+"&y="+latitude+"&input_coord=WGS84",
         	beforeSend: function (header) {
-        		header.setRequestHeader("Authorization","KakaoAK 840539f3651afe19f12cc19a1dc9e0ab");
+        		header.setRequestHeader("Authorization","KakaoAK ");
             },
             success:function(result){
             	var address=result.documents[0].address.address_name;
@@ -48,7 +48,7 @@
 			        	type:"get",
 			        	url:"https://dapi.kakao.com/v2/local/geo/coord2address.json?x="+latlng.La+"&y="+latlng.Ma+"&input_coord=WGS84",
 			        	beforeSend: function (header) {
-			        		header.setRequestHeader("Authorization","KakaoAK 840539f3651afe19f12cc19a1dc9e0ab");
+			        		header.setRequestHeader("Authorization","KakaoAK ");
 			            },
 			            success:function(clickresult){
 			            	var clickaddress=clickresult.documents[0].address.address_name;
